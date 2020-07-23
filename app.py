d2ae073b15766c31c8670a60754c1d4ebc4817e7
Tyler <tjwilson5634@gmail.com>
2020-07-22 23:39:36 -0600

    Flask

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 # import necessary libraries
-import MySQLdb
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
+from models import db
+
 
 # create instance of Flask app
 app = Flask(__name__)
 
 # Connection to SQL Database
-conn = MySQLdb.connect("localhost","postgres","Family34","Project2")
-cursor = conn.cursor()
- 
+db = SQLAlchemy(app)
+
+#Postgres credientials
+POSTGRES = {
+    'user': 'tj',
+    'pw': 'yourpass',
+    'db': 'Project2',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+# ...app config...
+db.init_app(app)
+
 
 # create route that renders index.html template
 @app.route("/")
+
 def example():
     cursor.execute("select * from mmy")
     data = cursor.fetchall() #data from database.
