eca7e0b9529ac315eec90874d3f8d58dccfcfdfc
Tyler <tjwilson5634@gmail.com>
2020-07-22 19:53:27 -0600

    stephen

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # import necessary libraries
 import MySQLdb
 from flask import Flask, render_template
-import pandas as pd 
 
 # create instance of Flask app
 app = Flask(__name__)
@@ -14,21 +13,9 @@ cursor = conn.cursor()
 # create route that renders index.html template
 @app.route("/")
 def example():
-cursor.execute("select * from mmy")
-data = cursor.fetchall() #data from database.
-return render_template("index.html", value=data)
-
-@app.route("/scrape")
-def scrape():
-    # Run the scrape function
-    mars_data = scrape_mars.scrape_all()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/", code=302)
-
+    cursor.execute("select * from mmy")
+    data = cursor.fetchall() #data from database.
+    return render_template("index.html", value=data)
 
 
 if __name__ == "__main__":
