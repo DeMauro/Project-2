927bcadf0eec2032be2ed6b2365b3535053b7f30
Tyler <tjwilson5634@gmail.com>
2020-07-24 11:14:43 -0600

    Flask

--- app.py
+++ app.py
@@ -18,38 +18,29 @@ cursor = connection.cursor()
 
 
 # create route that renders index.html template
+
 @app.route("/")
-def example():
-    postgreSQL_select_Query = "select * from mmy"
+def test():
+    postgreSQL_select_Query = "SELECT cl.Make, cl.Model, cl.Year, cl.craigslist_price, cl.lat, cl.lng, c.mpg FROM craigs_list cl JOIN cars c on cl.make = c.make AND cl.model = c.model AND cl.year = c.year"
     cursor.execute(postgreSQL_select_Query)
-    records = cursor.fetchall() 
+    records= cursor.fetchall()
     car_data=[]
     car_dics={}
-
-    for record in records:
-        car_dics["Make"]= record[0]
-        car_dics["Model"]= record[1]
-        car_dics["Year"]= record[2]
     
-        car_data.append(car_dics.copy())
+    for record in records:
+        car_dics["Make"]= record[0]
+        car_dics["Model"]= record[1]
+        car_dics["Year"]= record[2]
+        car_dics["craigslist_price"]= record[3]
+        car_dics["lat"]= record[4]
+        car_dics["lng"]= record[5]
+        car_dics["mpg"]= record[6]
 
-    return jsonify(car_data)
-
-    postgreSQL_select_Query = "select * from cars"
-    cursor.execute(postgreSQL_select_Query)
-    records = cursor.fetchall() 
-    car_data=[]
-    car_dics={}
-
-    for record in records:
-        car_dics["Make"]= record[0]
-        car_dics["Model"]= record[1]
-        car_dics["Year"]= record[2]
-    
-        car_data.append(car_dics.copy())
+    print(car_dics)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
